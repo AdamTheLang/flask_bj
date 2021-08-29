5137a9ed971db13824ca33b50533447a70ffb46c
AdamTheLang <alang@lap-tech.com>
2021-08-29 16:23:21 -0700

    Working find_org page

--- project/forms.py
+++ project/forms.py
@@ -44,14 +44,37 @@ POPULATIONS = (
 )
 
 
-def isint(form, field):
-    int(field.data)
+def is_phone(form, field):
+    digits = ''.join(filter(str.isdigit, field.data))
+    if not (1999999999 < int(digits) < 19999999999):
+        raise validators.ValidationError("Invalid telephone number.")
+    return True
+
+
+class OrganizationSearchForm(FlaskForm):
+    org_name = StringField('Org Name', [validators.Length(min=4, max=40)])
+
+    state = SelectField(
+        label='State',
+        choices=[('Any', 'Any')] + [(state, state) for state in STATE_ABBREV]
+    )
+
+    issue = SelectField(
+        label='Primary Issues',
+        choices=(('Any', 'Any'),) + ISSUES,
+    )
+
+    population = SelectField(
+        label='Populations Served',
+        choices=(('Any', 'Any'),) + POPULATIONS
+    )
+
 
 
 class OrganizationEditForm(FlaskForm):
     id = IntegerField()
     org_name = StringField('Org Name', [validators.Length(min=4, max=40)])
-    org_url = StringField('Org URL', [validators.Length(min=4, max=200)])
+    org_url = StringField('Org URL', [validators.Length(max=200)])
 
     state = SelectField(
         label='State',
@@ -70,12 +93,12 @@ class OrganizationEditForm(FlaskForm):
 
     actions = TextAreaField(
         label='Typical Actions',
-        validators=[validators.Length(min=4, max=1200)],
+        validators=[validators.Length(max=1200)],
     )
 
     bj_contact = StringField(
         label='BJ Contact for Call',
-        validators=[validators.Length(min=4, max=40)]
+        validators=[validators.Length(max=40)]
     )
 
     first_contact = DateField(
@@ -88,30 +111,30 @@ class OrganizationEditForm(FlaskForm):
 
     followup = TextAreaField(
         label='Followup Actions',
-        validators=[validators.Length(min=4, max=1200)]
+        validators=[validators.Length(max=1200)]
     )
 
     notes = TextAreaField(
         label='Other Notes',
-        validators=[validators.Length(min=4, max=1200)]
+        validators=[validators.Length(max=1200)]
     )
 
     contact_name = StringField(
         label='Contact Name',
-        validators=[validators.Length(min=4, max=200)]
+        validators=[validators.Length(max=200)]
     )
 
     contact_title = StringField(
         label='Contact Title',
-        validators=[validators.Length(min=4, max=200)]
+        validators=[validators.Length(max=200)]
     )
 
     contact_phone = StringField(
         label='Contact Phone',
-        validators=[validators.Length(min=10, max=11), isint]
+        validators=[is_phone]
     )
 
     contact_email = StringField(
         label='Email Address',
-        validators=[validators.Length(min=6, max=50), validators.Email(),]
+        validators=[validators.Length(max=50), validators.Email(),]
     )
--- project/main.py
+++ project/main.py
@@ -1,20 +1,23 @@
 # main.py
 
-from flask import abort, Blueprint, redirect, render_template
+from flask import abort, Blueprint, flash, redirect, render_template, request
 from flask_login import login_required, current_user
 
 from . import models, forms, db
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def index():
     return render_template('index.html')
+
 
 @main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
 
 @main.route('/edit_org/<org_id>', methods=['GET', 'POST'])
 @main.route('/edit_org/', methods=['GET', 'POST'])
@@ -39,3 +42,25 @@ def edit_org(org_id=None):
         return redirect('/org_find')
 
     return render_template('edit_org.html', form=form)
+
+
+@main.route('/find_org/', methods=['GET'])
+@main.route('/find_org', methods=['GET'])
+@login_required
+def find_org():
+    form = forms.OrganizationSearchForm(request.args)
+    print(form.state.data)
+    query = models.Groups.query
+    if form.org_name.data:
+        query = query.filter_by(org_name=form.org_name)
+    if form.state.data and form.state.data != 'Any':
+        query = query.filter_by(state=form.state.data)
+    if form.issue.data and form.issue.data != 'Any':
+        query = query.filter_by(issues=form.issue.data)
+    if form.population.data and form.issue.data != 'Any':
+        query = query.filter_by(populations=form.population.data)
+    print(query)
+    results = list(query.all())
+
+    return render_template('find_org.html', form=form, results=results)
+
